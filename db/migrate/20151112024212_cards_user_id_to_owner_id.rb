d7dab9b90ddd10e5bea2602a1e32b86faf397d3a
Kevin Kuoch <kevinkuoch0@gmail.com>
2015-11-30 22:12:44 -0500

    refined migrations

--- db/migrate/20151112024212_cards_user_id_to_owner_id.rb
+++
@@ -1,5 +0,0 @@
-class CardsUserIdToOwnerId < ActiveRecord::Migration
-  def change
-  	change_column :cards, :user_id, :card_owner_id
-  end
-end
--- db/migrate/20151112025024_cards.rb
+++
@@ -1,4 +0,0 @@
-class Cards < ActiveRecord::Migration
-  def change
-  end
-end
